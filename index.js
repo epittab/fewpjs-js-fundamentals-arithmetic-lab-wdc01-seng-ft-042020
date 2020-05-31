dea972dedca28b601a8801bf3fc16d75d5c9e4d8
epittab <ep7m@me.com>
2020-05-31 16:18:00 -0400

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
 
 // Write your code here
+
+// Assignment 1
+let num1 = 2;
+let num2 = 31;
+let multiply = num1 * num2;
+
+//Assignment 2
+let random = Math.ceil(Math.random()* 10);
+
+//Assignment 3
+let mod = 9 % 5;
+
+//Assignment 4
+const someSet = [1, 4, 17, 10, 12, 20, 14];
+
+let max = Math.max(...someSet)
